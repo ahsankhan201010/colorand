7f9c3c28f104e6a4b12600392b7a1fdaf666de7e
ahsankhan201010 <ahsankhan201010@gmail.com>
2021-04-18 00:36:47 +0500

    colorand frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  var [color, setColor] = useState({
+    red: 40,
+    green: 44,
+    blue: 52,
+  });
+  var { red, green, blue } = color;
+
+  const changeColor = async () => {
+    try {
+      //fetch color from server
+      //set new color
+      console.log("fetch color")
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+        transition: "all 300ms ease-in",
+      }}
+    >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={changeColor}>Change Color</button>
       </header>
     </div>
   );
